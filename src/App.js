2e198a24f911a10ee641e0043462a5843acfdc71
EstelleDab <152979607+EstelleDab@users.noreply.github.com>
2025-02-17 10:16:21 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenue sur Moodmeter</h1>
+      <img class="logo" src="/images/logo.png" alt="logo"></img>
+    </div>
+  );
+}
+
+export default App;
