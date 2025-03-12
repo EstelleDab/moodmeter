c0be28e26d8c4819bb2238510a9e6d9b3547cfbf
Abigail <abigail.germon@etu.unilim.fr>
2025-03-12 19:22:09 +0100

    Ajout du tableau-de-bord prof

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.js";
 import StudentHome from "./components/StudentHome.js";
 import FeedbackForm from "./components/FeedbackForm.js";
+import Dashboard from './components/Dashboard';
 import Home from "./components/Home.js";
 import Student from "./models/Student.js";
 import "./styles/Global.css";
-import './bootstrap.css'; // Ensure the path is correct
-import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JavaScript
+import './bootstrap.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min'; 
 
 
-// Instance d'étudiant pour démonstration avec les cours par défaut
+
 const testStudent = new Student("Jane Doe", "jane@example.com", "12345", "/images/user.png", ["UE L315", "UE L316", "UE L317"]);
 console.log('testStudent courses:', testStudent.courses);
 const App = () => {
   return (
     <div>
 
-      {/* TestHome for testing purpose */}
+      {}
       <Router>
         <Routes>
           <Route path="/" element={<Home student={testStudent} />} />
           <Route path="/feedback" element={<FeedbackForm />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/studenthome" element={<StudentHome studentName={testStudent.name} studentPic={testStudent.pic} studentCourses={testStudent.courses} />} />
         </Routes>
       </Router>
