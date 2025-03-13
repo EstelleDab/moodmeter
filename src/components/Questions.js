521ec50407fdc791a623ebbd2cb5448bb951145f
Amanda <amanda.heryprieto@etu.unilim.fr>
2025-03-13 12:30:16 +0200

    Insertion du feedback dans la bdd

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,45 +1,44 @@
 const questions = {
-    travail: {
-      question: "Charge de travail",
-      options: {
-        1: { icon: '⛱️', label: 'Tranquille' },
-        2: { icon: '🙂', label: 'Je gère' },
-        3: { icon: '😌', label: 'Bien équilibré' },
-        4: { icon: '😕', label: 'En difficulté' },
-        5: { icon: '⚰️', label: 'Sous l\'eau' },
-      },
+  clarte_consigne: {
+    question: "Clarté des consignes",
+    options: {
+      1: { icon: '😫', label: 'Trop confus' },
+      2: { icon: '😵‍💫', label: 'Assez confus' },
+      3: { icon: '🤨', label: 'Moyennement clair' },
+      4: { icon: '🙂', label: 'Plutôt clair' },
+      5: { icon: '🤗', label: 'Très clair' },
     },
-    consignes: {
-      question: "Clarté des consignes",
-      options: {
-        1: { icon: '🤗', label: 'Très clair' },
-        2: { icon: '🙂', label: 'Plutôt clair' },
-        3: { icon: '🤨', label: 'Moyennement clair' },
-        4: { icon: '😵‍💫', label: 'Assez confus' },
-        5: { icon: '😫', label: 'Trop confus' },
-      },
+  },
+  difficulte: {
+    question: "Difficulté du cours",
+    options: {
+      1: { icon: '🤯', label: 'Trop difficile' },
+      2: { icon: '😵', label: 'Un peu dur' },
+      3: { icon: '🤓', label: 'Correct' },
+      4: { icon: '😊', label: 'Facile' },
+      5: { icon: '😎', label: 'Trop facile' },
     },
-    difficulte: {
-      question: "Difficulté du cours",
-      options: {
-        1: { icon: '😎', label: 'Trop facile' },
-        2: { icon: '😊', label: 'Facile' },
-        3: { icon: '🤓', label: 'Correct' },
-        4: { icon: '😵', label: 'Un peu dur' },
-        5: { icon: '🤯', label: 'Trop difficile' },
-      },
+  },
+  reactivite_enseignant: {
+    question: "Réactivité de l'enseignant",
+    options: {
+      1: { icon: '👻', label: 'Pas réactif' },
+      2: { icon: '👎', label: 'Peu réactif' },
+      3: { icon: '🤷', label: 'Moyennement réactif' },
+      4: { icon: '👍', label: 'Réactif' },
+      5: { icon: '🚀', label: 'Très réactif' },
     },
-    global: {
-      question: "Ressenti global",
-      options: {
-        1: { icon: '🥳', label: 'Tout roule !' },
-        2: { icon: '😀', label: 'Plutôt bien' },
-        3: { icon: '🫡', label: 'Ça va' },
-        4: { icon: '😥', label: 'Pas top' },
-        5: { icon: '😰', label: 'SOS' },
-      },
+  },
+  ressenti_global: {
+    question: "Ressenti global",
+    options: {
+      1: { icon: '😰', label: 'SOS' },
+      2: { icon: '😥', label: 'Pas top' },
+      3: { icon: '🫡', label: 'Ça va' },
+      4: { icon: '😀', label: 'Plutôt bien' },
+      5: { icon: '🥳', label: 'Tout roule !' },
     },
-  };
-  
-  export default questions;
-  
+  },
+};
+
+export default questions;
